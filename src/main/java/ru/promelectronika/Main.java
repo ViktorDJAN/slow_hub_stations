14179e967f217a048888aa3148a789518d2b95a2
Viktor Kashtanov <pistoletviktor@gmail.com>
2025-05-19 11:26:13 +0300

    main methods corrected

--- src/main/java/ru/promelectronika/Main.java
+++ src/main/java/ru/promelectronika/Main.java
@@ -7,6 +7,7 @@ import com.intelligt.modbus.jlibmodbus.master.ModbusMaster;
 import functional.concrete_classes.ThreePhaseEnergyMeter;
 import functional.modbus.DeviceModbus;
 import ru.promelectronika.http.ServerHttp;
+import ru.promelectronika.logHandler.LogHandler;
 import ru.promelectronika.proxy_handlers.OcppProxyHandler;
 import ru.promelectronika.util_stuff.ColorKind;
 import ru.promelectronika.util_stuff.ColorTuner;
@@ -37,27 +38,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException, JoranException, InterruptedException {
 
-
-//
-//
-//        // INITIALIZATION
+//                  INITIALIZATION
         LoggerPrinter.turnOnLogConfigurator(true);
         LoggerPrinter.logAndPrint(ColorKind.PURPLE_BG_BLACK_TEXT, LoggerType.MAIN_LOGGER, "PROGRAM STARTED IN MAIN");
-//        // CONFIGURATIONS READING
+//         CONFIGURATIONS READING
         ConfigsFile.setConfigsBasedOnJSONFile("/home/root/chargingStation/configurations/configuration.json");
 //        ConfigsFile.setConfigsBasedOnJSONFile("slow_charging_station/home/root/chargingStation/configurations/configuration.json");
 
         //         ** HTTP **
         ServerHttp.startHttpServer("192.168.3.200", 3060);
         LoggerPrinter.logAndPrint(ColorKind.PURPLE_BG_BLACK_TEXT, LoggerType.MAIN_LOGGER, "Http Started..." + ServerHttp.getServer().getAddress());
-
-
-        ScheduledExecutorService scheduled_service = Executors.newScheduledThreadPool(7 + ConfigsFile.mode3_controller_addresses.size());
+        ScheduledExecutorService scheduled_service = Executors.newScheduledThreadPool(8 + ConfigsFile.mode3_controller_addresses.size());
 
         // CHARGE_POINT_INITIALIZATION
         var chargePointOcpp = new ChargePointOcpp(ConfigsFile.ocpp_server_address);
         OcppOperation.setChargePointOcpp(chargePointOcpp);
         scheduled_service.scheduleAtFixedRate(chargePointOcpp::connect, 20, 3000, TimeUnit.MILLISECONDS);
+
         // MODBUS DEVICES INITIALIZATION
         ModbusMaster master2 = DeviceModbus.initialModbusMasterTCP(ConfigsFile.outer_en_meter_ip_address);
         var outerEnMeter = new ThreePhaseEnergyMeter(ConfigsFile.outer_en_meter_id, master2, ConfigsFile.outer_en_meter_ip_address);
@@ -67,16 +64,16 @@ public class Main {
         rpcServer = new RpcServer(ConfigsFile.rpc_server_address, ConfigsFile.rpc_server_port);
         scheduled_service.scheduleAtFixedRate(new ServerStarter(rpcServer), 20, 5000, TimeUnit.MILLISECONDS);
 
-
-
         // KICK OFF ENERGY_METERS CALCULATING
         EnergyMeterProcess.createProcess(energyMeters, true, true);
         LoggerPrinter.logAndPrint(ColorKind.GREEN_BG_YELLOW_TEXT, LoggerType.MAIN_LOGGER, "ENERGY_METER_COUNTERS ARE STARTED");
 
         // GETTING AVAILABLE POWER OF THE BUILDING  kWT  (OUTER ENERGY_METER)
         scheduled_service.scheduleAtFixedRate(new CalculatingAvailableParamsProcess(ConfigsFile.outer_en_meter_id, EnergyMeterType.OUTER_ENERGY_METER, EnergyMeterKind.NON_BUILT_IN), 20, 650, TimeUnit.MILLISECONDS);
+
         // KICK OFF TRACKER FOR TRACKING CURRENT CHANGES
         scheduled_service.scheduleAtFixedRate(new PhaseCurrentChangesTracker(ConfigsFile.outer_en_meter_id, EnergyMeterType.OUTER_ENERGY_METER), 400, 500, TimeUnit.MILLISECONDS);
+
         // GETTING CONSUMING POWER BY THE SLOW_STATION  kWT  (INNER ENERGY_METER)
         for (String address : ConfigsFile.mode3_controller_addresses) {
             int embeddedEnMeterId = Integer.parseInt(address.substring(10));
@@ -87,13 +84,15 @@ public class Main {
         scheduled_service.execute(new OcppProxyHandler(chargePointOcpp));
 
         // START_CHARGING_CONTROLLERS
-        for (int i = 0; i < ConfigsFile.mode3_controller_addresses.size(); i++) {
-            String address = ConfigsFile.mode3_controller_addresses.get(i);
+        int stationId = 0;
+        for (String address : ConfigsFile.mode3_controller_addresses) {
             int embeddedEnMeterId = Integer.parseInt(address.substring(10));
-            var station = new SlowChargingStation(i + 1, embeddedEnMeterId, address, ConfigsFile.mode3_controller_port, ConfigsFile.connector_id
+            var station = new SlowChargingStation(++stationId, embeddedEnMeterId, address, ConfigsFile.mode3_controller_port, ConfigsFile.connector_id
                     , ConfigsFile.rpc_server_address, ConfigsFile.rpc_server_port);
-
-            scheduled_service.schedule(new StationRunProcess(station, chargePointOcpp), 4000, TimeUnit.MILLISECONDS);
+            LogHandler.loggerMain.info("STATION CREATED " + stationId + " : " + station);
+            ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+            service.execute(new StationRunProcess(station, chargePointOcpp));
         }
+
     }
 }
